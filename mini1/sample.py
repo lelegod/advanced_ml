6190637cfb267aa4c5817c5398a0d6f4f31535a3
lelegod <2021m622@kuas.ac.jp>
2026-03-02 19:59:00 +0100

    increase fontsize in plots, better sample file

--- mini1/sample.py
+++ mini1/sample.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda', 'mps'], help='torch device')
     parser.add_argument('--latent-dim', type=int, default=32, metavar='N', help='dimension of latent variable')
     parser.add_argument('--n-samples', type=int, default=5000, metavar='N', help='number of samples to be generated')
+    parser.add_argument('--generate', action='store_true', help='Set this flag to generate new samples before calculating FID')
 
     args = parser.parse_args()
     print('# Options')
@@ -23,7 +24,7 @@ if __name__ == "__main__":
     # Define VAE model
     model = create_vae_model(args.prior, args.latent_dim, device)
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    model_path = os.path.join(script_dir, f"model/{args.prior}/{args.prior}_vae_model.pt")
+    model_path = os.path.join(script_dir, f"model/{args.prior}/{args.prior}_vae_model_run0.pt")
 
     model.load_state_dict(torch.load(model_path, map_location=torch.device(device), weights_only=True))
     model.eval()
@@ -36,7 +37,11 @@ if __name__ == "__main__":
         samples_dir = os.path.join(script_dir, f"model/{args.prior}/samples")
         os.makedirs(samples_dir, exist_ok=True)
         
-        reshaped_samples = samples.view(args.n_samples, 1, 28, 28)
-        for i in range(args.n_samples):
-            sample_path = os.path.join(samples_dir, f"{args.prior}_sample_{i}.png")
-            save_image(reshaped_samples[i], sample_path)
+        if args.generate:
+            samples_dir = os.path.join(script_dir, f"model/{args.prior}/samples")
+            os.makedirs(samples_dir, exist_ok=True)
+            
+            reshaped_samples = samples.view(args.n_samples, 1, 28, 28)
+            for i in range(args.n_samples):
+                sample_path = os.path.join(samples_dir, f"{args.prior}_sample_{i}.png")
+                save_image(reshaped_samples[i], sample_path)
